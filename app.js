54bb93b5bce0856c7f3e49c9e42dffa3c4528fba
Joe Bigio <jvbigio@gmail.com>
2020-08-19 11:16:36 -0400

    Added enter key event

--- app.js
+++ app.js
@@ -4,16 +4,35 @@
 // delete task when click delete icon
 const input = document.getElementById('task')
 const todo = document.querySelector('.todo')
-
-input.addEventListener('input', e => {
-  console.log(e.target.value)
-})
+const taskList = document.querySelector('.task-item')
+// console.log(taskList)
+// input.addEventListener('input', e => {
+//   console.log(e.target.value)
+// })
 input.addEventListener('input', taskInput)
 
+// input.addEventListener('keydown', ({ key }) => {
+//   if (key === 'Enter') {
+//     event.preventDefault()
+//     console.log('Enter key pressed')
+//   }
+// })
+
+// function taskInput (e) {
+//   const userInput = e.target.value
+//   todo.innerHTML = `<li class="todo">${userInput} <button class="deleteButton"><i class="far fa-trash-alt"></i></button> </li>`
+//   todo.style.borderBottom = '1px solid orange'
+// }
 function taskInput (e) {
-  // todo.textContent = e.target.value // deletes the icon
-  todo.innerHTML = `<li class="todo">${e.target.value} <button class="deleteButton"><i class="far fa-trash-alt"></i></button> </li>`
-  todo.style.borderBottom = '1px solid orange'
+  const userInput = e.target.value
+  input.addEventListener('keydown', ({ key }) => {
+    if (key === 'Enter') {
+      event.preventDefault()
+      // console.log('Enter key pressed')
+      todo.innerHTML = `<li class="todo">${userInput} <button class="deleteButton"><i class="far fa-trash-alt"></i></button> </li>`
+      todo.style.borderBottom = '1px solid #d49a3f'
+    }
+  })
 }
 
 // todo.textContent = 'TEST'
